3f171122c0cb058e5dfc93a8d5cef29fa7a53938
Vitor Manuel Estrella <vitor.manuel.estrella@gmail.com>
2025-08-20 23:08:12 -0300

    routa de recomedação de jogos

--- expert-sys-games/main.py
+++ expert-sys-games/main.py
@@ -1,6 +1,40 @@
-# main.py
 from fastapi import FastAPI
 from pydantic import BaseModel
-from experta import *
+from typing import List
+import json
 
 app = FastAPI()
+
+class Preferencias(BaseModel):
+    genero: str
+    faixa_etaria: str
+
+
+
+def carregar_dados():
+    with open('data.json', 'r') as f:
+        dados = json.load(f)
+    return dados
+
+def recomendar_jogos(preferencias: Preferencias):
+    recomendacoes = []
+    for regra in regras:
+        condicao = regra["Condição"]
+        if (preferencias.genero.title() == condicao["Gênero"] and
+                preferencias.faixa_etaria.title() == condicao["Faixa Etária"]):
+            recomendacoes.extend(regra["Recomendação"])
+    return recomendacoes
+
+
+
+dados = carregar_dados()
+jogos = dados['jogos']
+regras = dados['regras']
+
+@app.post("/recomendar-jogos/")
+async def recomendar_jogos_api(preferencias: Preferencias):
+    jogos_recomendados = recomendar_jogos(preferencias)
+    if jogos_recomendados:
+        return {"Jogos Recomendados": jogos_recomendados}
+    else:
+        return {"Mensagem": "Nenhuma recomendação disponível para suas preferências."}
